d72eb65de96eb5995ed516f0eb3467a1fa7a1bdd
Madhur82954 <itzmadhurverma@gmail.com>
2023-06-05 11:10:22 +0530

    [Madhur] - Password Added

--- Program.cs
+++ Program.cs
@@ -6,12 +6,13 @@ namespace RegexPatternMatchingDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Phone-No : ");
+            Console.WriteLine("Enter Password : ");
             string name=Console.ReadLine();
             Console.WriteLine(name);
             //string namepattern="^[A-Z]{1}[a-z]{2,}$";
             //string namepattern =  "^[0-9a-zA-Z]+[._-]{0,2}[0-9a-zA-z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
-            string namepattern = "^[0-9]{2,}[ ][0-9]{10}$";
+            //string namepattern = "^[0-9]{2,}[ ][0-9]{10}$";
+            string namepattern = "[0-9a-zA-Z]{8,}";
             Regex regex = new Regex(namepattern);
             if (regex.IsMatch(name))
             {
