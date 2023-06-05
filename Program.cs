7ccd2d5b62d659f9b40bb1b8289aa3dcb1c30770
Madhur82954 <itzmadhurverma@gmail.com>
2023-06-05 10:43:30 +0530

    [Madhur] - First Name Added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegexPatternMatchingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name : ");
+            string name=Console.ReadLine();
+            Console.WriteLine(name);
+            string namepattern="^[A-Z]{1}[a-z]{2,}$";
+            //string namepattern =  "^[0-9a-zA-Z]+[._-]{0,2}[0-9a-zA-z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
+            //string namepattern = "^[0-9]{2,}[ ][0-9]{10}$";
+            Regex regex = new Regex(namepattern);
+            if (regex.IsMatch(name))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid");
+                Console.ResetColor();
+            }
+        }
+    }
+}
